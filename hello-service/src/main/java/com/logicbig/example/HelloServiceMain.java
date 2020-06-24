64a9558a1b8c898970efe98717c82d1b5d02c775
kethan-sai <ykethansai@gmail.com>
2020-06-23 22:26:56 -0500

    hystix service problem

---
+++ hello-service/src/main/java/com/logicbig/example/HelloServiceMain.java
@@ -0,0 +1,17 @@
+package com.logicbig.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HelloServiceMain{
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceMain.class, args);
+	}
+}
---
+++ hello-web-client-service/src/main/java/com/logicbig/example/HelloWebClientController.java
@@ -0,0 +1,34 @@
+package com.logicbig.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import java.time.LocalDateTime;
+
+
+@Controller
+public class HelloWebClientController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/")
+    @HystrixCommand(fallbackMethod = "failed")
+    public String handleRequest(Model model) {
+        //accessing hello-service
+        HelloObject helloObject = restTemplate.getForObject("http://hello-service/hello", HelloObject.class);
+        model.addAttribute("msg", helloObject.getMessage());
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello-page";
+    }
+   
+    public String failed(Model model) {
+    	
+    	System.out.println("failed");
+    	return "hello-page";
+    }
+}
