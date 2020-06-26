d1473ea99bf585e0802e5ad0657447719cbeeb2e
kethan-sai <ykethansai@gmail.com>
2020-06-26 10:57:56 -0500

    dashboard with actuator endpoint

--- hello-web-client-service/src/main/java/com/logicbig/example/HelloWebClientServiceMain.java
+++ hello-web-client-service/src/main/java/com/logicbig/example/HelloWebClientServiceMain.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableDiscoveryClient
+@EnableHystrixDashboard
 public class HelloWebClientServiceMain {
 
     @LoadBalanced
